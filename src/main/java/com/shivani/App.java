5d80386e1f8d39320f3c07d3a753a5b4a524ecbb
shivani5002 <shivani18905@gmail.com>
2025-11-18 20:50:17 +0530

    Update App.java for webhooks

--- src/main/java/com/shivani/App.java
+++ src/main/java/com/shivani/App.java
@@ -3,5 +3,6 @@ package com.shivani;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Shivani’s Java Maven Project!");
+        System.out.println("Webhooks enabled");
     }
 }
