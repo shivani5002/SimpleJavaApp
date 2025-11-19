8e546be65be5d820ac94801c9bf78470de13c06d
shivani5002 <shivani18905@gmail.com>
2025-11-19 13:06:43 +0530

    remove

--- src/main/java/com/shivani/App.java
+++ src/main/java/com/shivani/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Shivani’s Java Maven Project!");
         System.out.println("Webhooks enabling done");
-        System.out.println("Added webhook");
+        
     }
 }
